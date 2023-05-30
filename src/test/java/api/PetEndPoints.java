a9bb271024aec646cf54c94767186fc01553b8af
lakshmi1687 <84918978+lakshmi1687@users.noreply.github.com>
2023-05-30 22:03:48 +0530

    Add files via upload

---
+++ src/test/java/api/PetEndPoints.java
@@ -0,0 +1,175 @@
+package api.constants;
+
+import static io.restassured.RestAssured.given;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+//created to perform create,retrieve,update and delete operations
+
+public class PetEndPoints {
+	
+	public static Response addPet() throws FileNotFoundException {
+    	
+        File f = new File(".\\petdata.json");
+		
+		FileReader fr = new FileReader(f);
+		
+		JSONTokener jt = new JSONTokener(fr);
+		
+		JSONObject jo = new JSONObject(jt);
+	
+				
+		Response response = given()
+		                     .contentType("application/json")
+		                     .accept("application/json")
+		                     .body(jo.toString())
+		
+		                   .when()
+		                       .post(Routes.postPetById);
+		
+		return response;
+		                       
+	}
+    public static void addPetByImage()  {
+    	
+    	String apiUrl = Routes.postPetImage;  
+        String imagePath = "C:\\Users\\Lakshmi.Pathi\\Documents\\dog3.jpg";  
+
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(apiUrl);
+
+        File imageFile = new File(imagePath);
+        FileBody fileBody = new FileBody(imageFile, ContentType.IMAGE_JPEG);
+
+        HttpEntity entity = MultipartEntityBuilder.create()
+                .addPart("image", fileBody)  
+                .build();
+
+        httpPost.setEntity(entity);
+
+        try {
+            HttpResponse response1 = httpClient.execute(httpPost);
+            HttpEntity responseEntity = response1.getEntity();
+
+            
+            if (responseEntity != null) {
+                String responseString = EntityUtils.toString(responseEntity);
+                System.out.println(responseString);
+            }
+
+            EntityUtils.consume(responseEntity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		                       
+	}
+	
+	public static Response getPetById(int petId) {
+		
+		Response response = given()
+		                      .pathParam("petId", petId)
+		                    .when()
+		                      .get(Routes.getPetById);
+		
+		        return response;
+		  
+	}
+	
+    public static Response getPetByStatus() {
+		
+		Response response = given()
+		                 
+		                    .when()
+		                      .get(Routes.getPetByStatus);
+		
+		        return response;
+		  
+	}
+    
+    public static Response updatePet() throws FileNotFoundException {
+    	
+        File f = new File(".\\UpdatedPetData.json");
+		
+		FileReader fr = new FileReader(f);
+		
+		JSONTokener jt = new JSONTokener(fr);
+		
+		JSONObject jo = new JSONObject(jt);
+	
+				
+		Response response = given()
+		                     .contentType("application/json")
+		                     .accept("application/json")
+		                     .body(jo.toString())
+		
+		                   .when()
+		                       .post(Routes.updatePet);
+		
+		return response;
+		                       
+	}
+	
+	public static Response deletePet(int petId) {
+		
+		Response response = given()
+		                      .pathParam("petId", petId)
+		                    .when()
+		                       .delete(Routes.delPetById);
+		
+		        return response;
+	}
+		
+	    public static RequestSpecification postPetByImage() {
+	    	RequestSpecification response = given();
+	        String apiUrl = Routes.postPetImage;  // Replace with the actual API endpoint URL
+	        String imagePath = "C:\\Users\\Lakshmi.Pathi\\Documents\\dog3.jpg";  // Replace with the actual path of your image file
+
+	        HttpClient httpClient = HttpClients.createDefault();
+	        HttpPost httpPost = new HttpPost(apiUrl);
+
+	        File imageFile = new File(imagePath);
+	        FileBody fileBody = new FileBody(imageFile, ContentType.IMAGE_JPEG);
+
+	        HttpEntity entity = MultipartEntityBuilder.create()
+	                .addPart("image", fileBody)  // "image" is the form field name for the image file
+	                .build();
+
+	        httpPost.setEntity(entity);
+
+	        try {
+	            HttpResponse response1 = httpClient.execute(httpPost);
+	            HttpEntity responseEntity = response1.getEntity();
+
+	            // Handle the response as needed
+	            if (responseEntity != null) {
+	                String responseString = EntityUtils.toString(responseEntity);
+	                System.out.println(responseString);
+	            }
+
+	            // Close resources
+	            EntityUtils.consume(responseEntity);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			return response;
+	    }
+
+
+
+}
