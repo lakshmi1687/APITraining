5e3a011eff4b0604e4fb2e4bbb59b1ed13efc12e
lakshmi1687 <84918978+lakshmi1687@users.noreply.github.com>
2023-06-01 14:47:29 +0530

    Update Scenario2.java

--- src/test/java/api/tests/Scenario2.java
+++ src/test/java/api/tests/Scenario2.java
@@ -1,37 +1,43 @@
 package api.tests;
 
-import java.io.FileNotFoundException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import org.testng.annotations.Test;
 
+import api.constants.Endpoints;
 import api.constants.PetEndPoints;
 import io.restassured.response.Response;
 
 public class Scenario2 {
 	@Test(priority=1)
-public void postPet() throws FileNotFoundException {
+public void postPet() {
 		
 		Response response = PetEndPoints.addPet();
 		
-		response.then().assertThat().statusCode(200)
+		response.then().assertThat().statusCode(is(equalTo(200)))
 		               .log().all();
 	}
 @Test(priority=2)
 public void getPetById() {
-	Response response = PetEndPoints.getPetById(3222);
-	      response.then().assertThat().statusCode(200);
-	      int id = response.jsonPath().get("category.id");
-	      if(id == 3101) {
-	    	  System.out.println("pet data is updated");
-	      } else {
-	    	  System.out.println("pet data is original");
-	      }
-			
+	  Endpoints idValue = Endpoints.ID;
+	  Endpoints updatedIdValue = Endpoints.ID1;
+	  
+		Response response = PetEndPoints.getPetById(idValue.getValue());
+		      response.then().assertThat().statusCode(is(equalTo(200)));
+		      int id = response.jsonPath().get("category.id");
+		      if(id == updatedIdValue.getValue()) {
+		    	  System.out.println("pet data is updated");
+		      } else {
+		    	  System.out.println("pet data is original");
+		      }
+
 }
 @Test(priority=3)
 public void getPetByStatus() {
 	Response response = PetEndPoints.getPetByStatus();
-	      response.then().assertThat().statusCode(200)
+	      response.then().assertThat().statusCode(is(equalTo(200)))
 	     .log().all();
 	               	
 }
@@ -39,7 +45,7 @@ public void getPetByStatus() {
 public void delPet() {
 	Response response = PetEndPoints.deletePet(3222);
 	
-	response.then().assertThat().statusCode(200);
+	response.then().assertThat().statusCode(is(equalTo(200)));
 }
 
 }
